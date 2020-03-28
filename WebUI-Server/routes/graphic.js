d1c61d058ae74923976b7521931b488f21530bcb
Hinovska <andresgomez456@gmail.com>
2020-03-28 13:15:38 -0500

    Eliminacion de proyectos experimentales

--- WebUI-Server/routes/graphic.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('graphic', { title: 'Prometheus Proyect.', message: "Remote car moved by myoelectric signals." });
-});
-
-module.exports = router;
